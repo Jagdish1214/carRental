4b85b0ad020e459770b25505ebfbad93b550be88
Jagdish1214 <jagdishshinde362@gmail.com>
2026-03-08 15:04:39 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://urbanrentals-backend-production.up.railway.app",
   withCredentials: true, // ⭐ sends JSESSIONID cookie
 });
 
